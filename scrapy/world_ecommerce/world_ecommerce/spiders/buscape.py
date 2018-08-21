a161a482505c730afe158ca5509bfd7001420074
rafamph <30378497+rafamph@users.noreply.github.com>
2018-08-20 23:49:55 -0500

    Add files via upload

--- scrapy/world_ecommerce/world_ecommerce/spiders/buscape.py
+++ scrapy/world_ecommerce/world_ecommerce/spiders/buscape.py
@@ -8,7 +8,16 @@ class BuscapeSpider(scrapy.Spider):
     start_urls = ['http://buscape.com.br/search/tv-led/']
 
     def parse(self, response):
-        names = response.xpath('//*[@class="card--product__name u-truncate-multiple-line"]/text()').extract()
+        names = response.xpath('//*[@itemprop="name"]/text()').extract()
         images = response.xpath('//*[@class="card--product__thumb"]/@src').extract()
         prices = response.xpath('//*[@itemprop="lowPrice"]/@content').extract()
-        yield{"names" : names[0], "images:" : images[0], "prices:" : prices[0]}
+        for i in range(len(names)):
+            yield{"names:" : names[i], "images:" : images[i], "prices:" : prices[i]}
+
+
+	#yield{"names" : names[0], "images:" : images[0], "prices:" : prices[0]}
+	#new_pages = response.xpath('//*[@class="button--link__checkout btn-neemu-addtocart"]/@data-link').extract() 
+        #for new_page in new_pages:
+            #new_url = response.urljoin(new_page)
+            #new_response = (scrapy.Request(new_url))
+            

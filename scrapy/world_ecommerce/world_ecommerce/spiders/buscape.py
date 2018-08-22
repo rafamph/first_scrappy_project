594db5209406db8faecba8c3769fdf05dad72d53
rafamph <30378497+rafamph@users.noreply.github.com>
2018-08-21 23:59:32 -0500

    Add files via upload

--- scrapy/world_ecommerce/world_ecommerce/spiders/buscape.py
+++ scrapy/world_ecommerce/world_ecommerce/spiders/buscape.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from selenium import webdriver
+from scrapy.selector import Selector
 
 class BuscapeSpider(scrapy.Spider):
     name = 'buscape'
@@ -9,11 +10,18 @@ class BuscapeSpider(scrapy.Spider):
 
     def parse(self, response):
         names = response.xpath('//*[@itemprop="name"]/text()').extract()
-        images = response.xpath('//*[@class="card--product__thumb"]/@src').extract()
+        #images = response.xpath('//*[@class="card--product__thumb"]/@src').extract()
         prices = response.xpath('//*[@itemprop="lowPrice"]/@content').extract()
+        #for i in range(len(names)):
+            #yield{"names:" : names[i], "images:" : images[i], "prices:" : prices[i]}
+
+        driver = webdriver.Chrome('/home/raflicky/Downloads/chromedriver')
+        driver.get("buscape.com.br/search/tv-led")
+
+        images = driver.find_element_by_path('//*[@class="card--product__thumb"]/@src').extract()
+
         for i in range(len(names)):
             yield{"names:" : names[i], "images:" : images[i], "prices:" : prices[i]}
-
 
 	#yield{"names" : names[0], "images:" : images[0], "prices:" : prices[0]}
 	#new_pages = response.xpath('//*[@class="button--link__checkout btn-neemu-addtocart"]/@data-link').extract() 
